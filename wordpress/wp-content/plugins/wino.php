ee0cde3f21dec22553436d2fde485faa0c9d8b0a
Greg Linch <greglinch@gmail.com>
2013-07-01 23:34:17 -0400

    add wino plugin file

---
+++ wordpress/wp-content/plugins/wino.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Wino
+Plugin URI: 
+Description: Wine and dine all of the things!
+Version: 1.0
+Author: Greg Linch
+Author URI: http://www.greglinch.com
+License: GPL2
+*/
+?>
