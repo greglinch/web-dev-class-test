ade237cb77375a298f2b3f2836f5b1f5cd7cc2c8
Greg Linch <greglinch@gmail.com>
2013-07-02 18:39:29 -0400

    edit wino plugin

--- wordpress/wp-content/plugins/wino.php
+++ wordpress/wp-content/plugins/wino.php
@@ -8,4 +8,19 @@ Author: Greg Linch
 Author URI: http://www.greglinch.com
 License: GPL2
 */
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+	register_post_type( 'veoni_wine',
+		array(
+			'labels' => array(
+				'name' => __( 'Wines' ),
+				'singular_name' => __( 'Wine' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		)
+	);
+}
+
 ?>
